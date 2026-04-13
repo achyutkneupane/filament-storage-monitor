3171bcedf0da1e85af7612351ec6322566877cc0
achyutkneupane <achyutkneupane@gmail.com>
2026-04-13 19:31:19 +0545

    feat: adds FilamentStorageMonitorProvider and updates composer.json Includes autoloading for test classes and new scripts for testing.

---
+++ src/Providers/FilamentStorageMonitorProvider.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AchyutN\FilamentStorageMonitor\Providers;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+final class FilamentStorageMonitorProvider extends BaseServiceProvider {}
